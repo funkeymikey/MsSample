080a02331eee87a6095b3d75d6d276b42d0824ad
Mike Kozelsky <mike.kozelsky@gmail.com>
2013-01-18 10:22:30 -0500

    idfk

--- MsSample.Web/MsSample.js
+++ MsSample.Web/MsSample.js
@@ -1,22 +1,34 @@
-var MsSample;
-(function (MsSample) {
-    var Controller = (function () {
-        function Controller($scope) {
-            $scope.Name = "poopykins";
-            $scope.Restaurants = [
-                {
-                    Name: "one"
-                }, 
-                {
-                    Name: "two"
-                }
-            ];
-            var s = $scope;
-            $.get("http://localhost/MsSample.Api/Restaurants", function (data) {
-                s.Restaurants = data;
-            });
-        }
-        return Controller;
-    })();
-    MsSample.Controller = Controller;    
-})(MsSample || (MsSample = {}));
+angular.service('Wine', function ($resource) {
+    return $resource('http://localhost/MsSample.Api/Restaurants/', {}, {
+        update: { method: 'PUT' }
+    });
+});
+
+
+function WineListCtrl(Wine) {
+
+    this.wines = Wine.query();
+
+}
+
+function WineDetailCtrl(Wine) {
+
+    this.wine = Wine.get({ wineId: this.params.wineId });
+
+
+    this.saveWine = function () {
+        if (this.wine.id > 0)
+            this.wine.$update({ wineId: this.wine.id });
+        else
+            this.wine.$save();
+        window.location = "#/wines";
+    }
+
+    this.deleteWine = function () {
+        this.wine.$delete({ wineId: this.wine.id }, function () {
+            alert('Wine ' + wine.name + ' deleted')
+            window.location = "#/wines";
+        });
+    }
+
+}
--- MsSample.Web/MsSample.ts
+++ MsSample.Web/MsSample.ts
@@ -1,6 +1,15 @@
 /// <reference path="TypeScript Definitions/jquery/jquery-1.9.d.ts" />
 /// <reference path="TypeScript Definitions/angularjs/angular-1.0.d.ts" />
 
+
+angular.service('MsSample', function ($resource) {
+    return $resource('api/wines/:wineId', {}, {
+        update: {method:'PUT'}
+    });
+});
+
+
+
 module MsSample {
     
 export class Controller {
