075c6c71ecc9a4c10345409105f46eb89f28f03d
Mike Kozelsky <mike.kozelsky@gmail.com>
2013-01-22 17:33:03 -0500

    Comments and stuff

--- MsSample.Web/Restaurant.js
+++ MsSample.Web/Restaurant.js
@@ -1,9 +1,22 @@
 ﻿var restaurants = angular.module('Restaurants', ['ngResource']);
 
-restaurants.controller('RestaurantCtrl', function ($scope, RestaurantApi) {
-    $scope.restaurants = RestaurantApi.query();
+//register the path to our RESTful api as a constant to be used for dependency injection elsewhere
+restaurants.constant('ApiPath', 'http://localhost/MsSample.Api/Restaurants/:id')
+
+//create the $resource object which will communicate with our RESTful api.
+//factory = created once, only when necessary
+restaurants.factory('RestaurantApi', function ($resource, ApiPath) {
+    return $resource(ApiPath);
 });
 
-restaurants.factory('RestaurantApi', function ($resource) {
-    return $resource('http://localhost/MsSample.Api/Restaurants');
+//all the logic for our page
+restaurants.controller('RestaurantCtrl', function ($scope, RestaurantApi) {
+    $scope.restaurants = RestaurantApi.query();
+
+    
+    $scope.showData = function (restaurantId) {
+        $scope.currentRestaurant = RestaurantApi.get({ id: restaurantId });
+    };
 });
+
+
